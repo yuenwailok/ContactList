e9bf15fb7e5fda1daee1f9bdb2fc5d8a01c505b2
WaiLokYuen <wai.y@oscillosoft.com.au>
2025-10-19 13:03:54 +1100

    add unit test for update and delete update add unit test

--- ContactListTest/ContactListTest.cs
+++ ContactListTest/ContactListTest.cs
@@ -4,8 +4,11 @@ using ContactList.Repositories;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using Moq;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework.Legacy;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Results;
@@ -22,7 +25,7 @@ namespace ContactListTest
         public async Task GetAllControllerTest()
         {
             List<Contact> contacts = new List<Contact>();
-            for (int i = 1; i < 4;i++)
+            for (int i = 1; i < 4; i++)
             {
                 var contact = new Contact
                 {
@@ -43,29 +46,6 @@ namespace ContactListTest
             var result = ((OkObjectResult)allcontacts.Result).Value;
             mock.Verify(r => r.GetAllAsync());
             Assert.That(contacts, Is.EqualTo(result));
-        }
-
-        [Test]
-        public async Task AddControllerTest()
-        {
-            var contact = new Contact
-            {
-                ContactID = 1,
-                Address = "Test Street " + 1,
-                Email = "testest@gmail.com",
-                PhoneNumber = "444444" + 1,
-                FirstName = "Test",
-                LastName = "Test",
-                Notes = "Hello World"
-            };
-            int fakeId = 1;
-            var mock = new Mock<IContactRepository>();
-            var controller = new ContactsController(mock.Object);
-            var addcontacts = await controller.AddAsync(contact);
-            mock.Verify(r => r.AddAsync(contact));
-            var result = ((CreatedAtActionResult)addcontacts);
-            int status_code = 201;
-            Assert.That(result.StatusCode, Is.EqualTo(status_code));
         }
 
         [Test]
@@ -106,8 +86,105 @@ namespace ContactListTest
             Assert.That(contact, Is.EqualTo(result));
         }
 
+        [Test]
+        public async Task AddControllerTest()
+        {
+            var contact = new Contact
+            {
+                Address = "Test Street " + 1,
+                Email = "testest@gmail.com",
+                PhoneNumber = "444444" + 1,
+                FirstName = "TestAdd",
+                LastName = "Test",
+                Notes = "Hello World"
+            };
+
+            int fakeId = 1;
+            var mock = new Mock<IContactRepository>();
+            var controller = new ContactsController(mock.Object);
+            var addcontacts = await controller.AddAsync(contact);
+            mock.Verify(r => r.AddAsync(contact));
+            var result = ((CreatedAtActionResult)addcontacts);
+            var value = result.Value;
+            int status_code = 201;
+
+            Assert.That(value, Is.EqualTo(contact));
+            Assert.That(result.StatusCode, Is.EqualTo(status_code));
+        }
+
+        [Test]
+        public async Task UpdateControllerTest()
+        {
+            int wrongId = 999;
+            int realId = 1;
+            var contact = new Contact
+            {
+                ContactID = realId,
+                Address = "Test Street " + 1,
+                Email = "testest@gmail.com",
+                PhoneNumber = "444444" + 1,
+                FirstName = "TestAdd",
+                LastName = "Test",
+                Notes = "Hello World"
+            };
+            var mock = new Mock<IContactRepository>();
+            var controller = new ContactsController(mock.Object);
+            mock.Setup(r => r.GetByIdAsync(realId)).Returns(Task.FromResult<Contact>(contact));
+
+            var updatecontactsnull = await controller.Update(wrongId, contact);
+            var nullresult = ((NotFoundObjectResult)updatecontactsnull);
+            var status_code_null = 404;
+            mock.Verify(r => r.GetByIdAsync(wrongId));
+
+            Assert.That(status_code_null, Is.EqualTo(nullresult.StatusCode));
+
+            var updatecontacts = await controller.Update(realId, contact);
+            var result = ((NoContentResult)updatecontacts);
+            var status_code = 204;
+            mock.Verify(r => r.GetByIdAsync(realId));
+            mock.Verify(r => r.UpdateAsync(contact));
+
+            Assert.That(status_code, Is.EqualTo(result.StatusCode));
+
+
+        }
+
+        [Test]
+        public async Task DeleteControllerTest()
+        {
+            int wrongId = 999;
+            int realId = 1;
+            var contact = new Contact
+            {
+                ContactID = realId,
+                Address = "Test Street " + 1,
+                Email = "testest@gmail.com",
+                PhoneNumber = "444444" + 1,
+                FirstName = "TestAdd",
+                LastName = "Test",
+                Notes = "Hello World"
+            };
+            var mock = new Mock<IContactRepository>();
+            var controller = new ContactsController(mock.Object);
+            mock.Setup(r => r.GetByIdAsync(realId)).Returns(Task.FromResult<Contact>(contact));
+
+            var deletecontactsnull = await controller.Delete(wrongId);
+            var nullresult = ((NotFoundObjectResult)deletecontactsnull);
+            var status_code_null = 404;
+            mock.Verify(r => r.GetByIdAsync(wrongId));
+
+            Assert.That(status_code_null, Is.EqualTo(nullresult.StatusCode));
+
+            var deletecontacts = await controller.Delete(realId);
+            var result = ((NoContentResult)deletecontacts);
+            var status_code = 204;
+            mock.Verify(r => r.GetByIdAsync(realId));
+            mock.Verify(r => r.DeleteAsync(realId));
+
+            Assert.That(status_code, Is.EqualTo(result.StatusCode));
 
 
 
+        }
     }
 }
